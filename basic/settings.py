a06b453d01820b9c656d117ed2a95089d0b75cd6
PyCode Hub <ansh.varshneydec@gmail.com>
2022-12-08 19:12:03 +0530

    myprojectsss

--- basic/settings.py
+++ basic/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'basic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'y168ARYiZy5QLUaaSFTM',
+        'HOST': 'containers-us-west-152.railway.app',
+        'PORT': '5771',
     }
 }
 
